921c715b58db81dd504c6e077cde86a5b2cf9c8f
O <o@Darko-Baturs-iMac.local>
2018-06-30 13:53:31 +0200

    Add UI outlets

--- Daljinac/Scenes/Registration Scene/RegistrationView.swift
+++ Daljinac/Scenes/Registration Scene/RegistrationView.swift
@@ -8,14 +8,26 @@
 
 import UIKit
 
-class RegistrationView: UIView {
+class RegistrationView: UIView, UITextFieldDelegate {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    @IBOutlet var registrationView: RegistrationView!
+    @IBOutlet weak var telegrafLogo: UIButton!
+    @IBOutlet weak var daljinacLogo: UIImageView!
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var daljinacLink: UILabel!
+    @IBOutlet weak var firstNameTextFild: UITextField!
+    @IBOutlet weak var lastNameTxtFild: UITextField!
+    @IBOutlet weak var phoneNumberTxtFild: UITextField!
+    @IBOutlet weak var passwordTxtFild: UITextField!
+    @IBOutlet weak var rePasswordTxtFild: UITextField!
+    @IBOutlet weak var termsLbl: UILabel!
+    @IBOutlet weak var termsButton: UIButton!
+    @IBOutlet weak var registrationButton: UIButton!
+    @IBOutlet weak var ChanelListButton: UIButton!
+    @IBOutlet weak var aboutUsButton: UIButton!
+    @IBOutlet weak var supportButton: UIButton!
+    @IBOutlet weak var usingTermsButton: UIButton!
+    
+    
 
 }
