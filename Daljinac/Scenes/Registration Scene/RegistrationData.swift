632f14104523c7cc9dd2b5ce3656fe34e04a643b
O <o@Darko-Baturs-iMac.local>
2018-06-30 12:54:15 +0200

    Add registratinData Struct

---
+++ Daljinac/Scenes/Registration Scene/RegistrationData.swift
@@ -0,0 +1,19 @@
+//
+//  RegistrationData.swift
+//  Daljinac
+//
+//  Created by O on 6/30/18.
+//  Copyright © 2018 Pedja Jevtic. All rights reserved.
+//
+
+import Foundation
+
+struct RegistrationData {
+    
+     let firstName: String?
+     let lastName: String?
+     let phoneNumber: String?
+     let password: String?
+     let repassword: String?
+     let termsAccsepting: Bool?
+}
