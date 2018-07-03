dc1639cdf5d5811c8aad914948fb3a0126c71abf
O <o@Darko-Baturs-iMac.local>
2018-07-03 05:18:26 +0200

    add chanalList funcx

--- Daljinac/Scenes/Registration Scene/RegistrationViewController.swift
+++ Daljinac/Scenes/Registration Scene/RegistrationViewController.swift
@@ -41,8 +41,11 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
         
     }
     
+    // MARK: - Terms and information button func
     
     @IBAction func checkBoxTapped(_ sender: UIButton) {
+        
+        // animation for chckbox
         
 //        UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
 //            sender.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
@@ -52,6 +55,7 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
 //                sender.transform = .identity
 //            }, completion: nil)
 //        }
+        // default chckbox
         
         if sender.isSelected {
          sender.isSelected = false
@@ -61,9 +65,24 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
         
     }
     
+}
+
+@IBAction func chanelListTap(_ sender: Any) {
     
+    print("Spisak kanla")
+    
+    
+}
+
+@IBAction func aboutUsTap(_ sender: Any) {
+    
+    print("O nama")
+    
+}
+
+@IBAction func supportTap
         
-    }
+
     
 
     
